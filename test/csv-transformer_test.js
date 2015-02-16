fb6864882c3fbb863233583ff1ece4c7b45684eb
Bill Ingram <waingram@gmail.com>
2015-02-16 10:41:12 -0600

    add more tests

--- test/csv-transformer_test.js
+++ test/csv-transformer_test.js
@@ -1,20 +1,138 @@
 /*global describe,it*/
 'use strict';
 
-var 
-  assert = require('assert'),
-  csvw  = require('../lib/csv-transformer.js'),
-  csvUrl = 'https://raw.githubusercontent.com/iherman/CSVPlus/gh-pages/tests/tree-ops/tree-ops.csv';
+var assert = require('assert'),
+    request = require('request'),
+    CSVTransformer = require('../lib/csv-transformer.js');
 
-describe('csvw node module', function() {
-  it('must parse four (4) rows', function() {
-    var options = {
-      csv: csvUrl
-    };
-    csvw.parse(options, function (json) {
-      console.log(json);
-      assert(json.length === 4);
+
+describe('Simple mapping', function () {
+  it('Test001: The simplest possible table without metadata', function (done) {
+    var csv = 'http://w3c.github.io/csvw/tests/test001.csv',
+        actual = '',
+        expected = { url: 'http://w3c.github.io/csvw/tests/test001.csv',
+          distribution: { downloadURL: 'http://w3c.github.io/csvw/tests/test001.csv' },
+          row:
+              [ { Surname: 'Homer', FamilyName: 'Simpson' },
+                { Surname: 'Marge', FamilyName: 'Simpson' },
+                { Surname: 'Bart', FamilyName: 'Simpson' },
+                { Surname: 'Lisa', FamilyName: 'Simpson' },
+                { Surname: 'Maggie', FamilyName: 'Simpson' },
+                { Surname: 'Ned', FamilyName: 'Flanders' },
+                { Surname: 'Krusty', FamilyName: 'the Clown' },
+                { Surname: 'Waylon', FamilyName: 'Smithers' } ] },
+        options = {csv: csv},
+        csvTransformer = new CSVTransformer(options);
+
+    request.get(csv).pipe(csvTransformer);
+
+    csvTransformer.on('data', function (chunk) {
+      actual += chunk;
     });
+
+    csvTransformer.on('finish', function () {
+      actual = JSON.parse(actual);
+      assert.deepEqual(actual, expected);
+      done();
+    });
+
+  });
+
+  it('Test002: Table with one quoted field without metadata', function (done) {
+    var csv = 'http://w3c.github.io/csvw/tests/test002.csv',
+        actual = '',
+        expected = { url: 'http://w3c.github.io/csvw/tests/test002.csv',
+          distribution: { downloadURL: 'http://w3c.github.io/csvw/tests/test002.csv' },
+          row:
+              [ { Surname: 'Homer', FamilyName: 'Simpson' },
+                { Surname: 'Marge', FamilyName: 'Simpson' },
+                { Surname: 'Bart', FamilyName: 'Simpson' },
+                { Surname: 'Lisa', FamilyName: 'Simpson' },
+                { Surname: 'Maggie', FamilyName: 'Simpson' },
+                { Surname: 'Ned', FamilyName: 'Flanders' },
+                { Surname: 'Krusty', FamilyName: 'the Clown' },
+                { Surname: 'Waylon', FamilyName: 'Smithers' } ] },
+        options = {csv: csv},
+        csvTransformer = new CSVTransformer(options);
+
+    request.get(csv).pipe(csvTransformer);
+
+    csvTransformer.on('data', function (chunk) {
+      actual += chunk;
+    });
+
+    csvTransformer.on('finish', function () {
+      actual = JSON.parse(actual);
+      assert.deepEqual(actual, expected);
+      done();
+    });
+
+  });
+
+  it('Test003: Table with whitespace before and after every field without metadata', function (done) {
+    var csv = 'http://w3c.github.io/csvw/tests/test003.csv',
+        actual = '',
+        expected = { url: 'http://w3c.github.io/csvw/tests/test003.csv',
+          distribution: { downloadURL: 'http://w3c.github.io/csvw/tests/test003.csv' },
+          row:
+              [ { Surname: ' Homer ', FamilyName: ' Simpson ' },
+                { Surname: ' Marge ', FamilyName: ' Simpson ' },
+                { Surname: ' Bart ', FamilyName: ' Simpson ' },
+                { Surname: ' Lisa ', FamilyName: ' Simpson ' },
+                { Surname: ' Maggie ', FamilyName: ' Simpson ' },
+                { Surname: ' Ned ', FamilyName: ' Flanders ' },
+                { Surname: ' Krusty ', FamilyName: ' the Clown ' },
+                { Surname: ' Waylon ', FamilyName: ' Smithers ' } ] },
+        options = {csv: csv},
+        csvTransformer = new CSVTransformer(options);
+
+    request.get(csv).pipe(csvTransformer);
+
+    csvTransformer.on('data', function (chunk) {
+      actual += chunk;
+    });
+
+    csvTransformer.on('finish', function () {
+      actual = JSON.parse(actual);
+      assert.deepEqual(actual, expected);
+      done();
+    });
+
+  });
+
+  it('Test005: A table with entity identifiers and references to other entities without metadata', function (done) {
+    var csv = 'http://w3c.github.io/csvw/tests/test005.csv',
+        actual = '',
+        expected = { url: 'http://w3c.github.io/csvw/tests/test005.csv',
+          distribution: { downloadURL: 'http://w3c.github.io/csvw/tests/test005.csv' },
+          row:
+              [ {"id": "1", "Surname": "Homer", "FamilyName": "Simpson", "child_id": "3"},
+                {"id": "1", "Surname": "Homer", "FamilyName": "Simpson", "child_id": "4"},
+                {"id": "1", "Surname": "Homer", "FamilyName": "Simpson", "child_id": "5"},
+                {"id": "2", "Surname": "Marge", "FamilyName": "Simpson", "child_id": "3"},
+                {"id": "2", "Surname": "Marge", "FamilyName": "Simpson", "child_id": "4"},
+                {"id": "2", "Surname": "Marge", "FamilyName": "Simpson", "child_id": "5"},
+                {"id": "3", "Surname": "Bart", "FamilyName": "Simpson", "child_id": null},
+                {"id": "4", "Surname": "Lisa", "FamilyName": "Simpson", "child_id": null},
+                {"id": "5", "Surname": "Maggie", "FamilyName": "Simpson", "child_id": null},
+                {"id": "6", "Surname": "Ned", "FamilyName": "Flanders", "child_id": null},
+                {"id": "7", "Surname": "Krusty", "FamilyName": "the Clown", "child_id": null},
+                {"id": "8", "Surname": "Waylon", "FamilyName": "Smithers", "child_id": null} ] },
+        options = {csv: csv, null: null},
+        csvTransformer = new CSVTransformer(options);
+
+    request.get(csv).pipe(csvTransformer);
+
+    csvTransformer.on('data', function (chunk) {
+      actual += chunk;
+    });
+
+    csvTransformer.on('finish', function () {
+      actual = JSON.parse(actual);
+      console.log(actual);
+      assert.deepEqual(actual, expected);
+      done();
+    });
+
   });
 });
-
